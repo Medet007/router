71eccdbcda27d8392773ddf7837d390bac5909c5
medet <malyka.maryam@gmail.com>
2019-01-26 20:53:38 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+//импортирование библиотеки
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Login from  "./views/login/login";
+import About from "./views/about/about";
+import Profile from "./views/profile/profile";
+import 'bootstrap/dist/css/bootstrap.min.css';// импорт bootstrap css в наш поект
+import NotFound from "./views/404";
+//import './App.css';
+class App extends Component {
+	//конструктор
+	constructor(props){
+		super(props);
+		this.state={
+			users:[{login:"login", password:"pass"}]//возможные комбинации логина и пароля
+		}
+	}
+  render() {
+    return (
+      <div className="App">
+        <Router>
+					<Switch>
+						<Route exact path="/" component= {About} />
+						<Route path="/profile" render = {(props)=><Profile name="somename"/>} />
+						<Route path="/login"   render  = {(props)=><Login  users={this.state.users}/>}/>
+						<Route component={NotFound}/>
+					</Switch>
+				</Router>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/views/404.js
@@ -0,0 +1,9 @@
+import React from "react";
+const NotFoynd = (props)=>{
+  return <div>
+  <h1> 404</h1>
+  <h2>stranica ne naidena!</h2>
+</div>
+
+}
+export default NotFoynd
